6140ba1a01d7dd353d04b503a286080ab747ee82
Christian <christian.almeida@bandtec.com.br>
2021-10-06 18:08:36 -0300

    refactor:login de usuarios e tatuadores

--- API-Ink-4-You/ink-4-you/src/main/java/br/com/bandtec/ink4yousembanco/repository/TatuadorRepository.java
+++ API-Ink-4-You/ink-4-you/src/main/java/br/com/bandtec/ink4yousembanco/repository/TatuadorRepository.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TatuadorRepository extends JpaRepository<Tatuador, Integer> {
-    @Query("select t from Tatuador t where t.email = ?1 and t.senha = ?2")
     Tatuador findByEmailAndSenha(String email, String senha);
 }
